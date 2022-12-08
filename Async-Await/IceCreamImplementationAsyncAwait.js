c12a1dfb138e4df5b1b755c0e9b807faf933281e
carlkarama <48504139+carlkarama@users.noreply.github.com>
2022-12-08 15:02:28 +1100

    feat: kitchen async await implementation

--- Async-Await/IceCreamImplementationAsyncAwait.js
+++ Async-Await/IceCreamImplementationAsyncAwait.js
@@ -16,4 +16,26 @@ let toppingsChoice = () => {
     });
 };
 
-kitchen()
+/**
+ * Imagine you are the chef of the restaurant & you make ice creams.
+ * You take an order from the customer and realise the customer hasn't told you what toppings you'd like
+ * So you have to go outside the kitchen and ask the customer which topping they'd like (chocolate or peanut).  
+ * Then he gives you the order hmm...give me chocolate.
+ * The you have to return to the kitchen and start working 
+ * 
+ * In summary, this process means you have to go outside, therefore, the kitchen is being stopped completely.
+ * Because without the chef the kitchen doesn't work. But realise there are other employees at the restaurant
+ * who are working, cleaning the tables, dishes, taking orders. 
+ * Everything else except the kitchen is working.
+ * 
+ */
+async function kitchen() {
+
+    try {
+        await abc();
+    } catch {
+        console.log("abc function doesn't exist");
+    } finally {
+        console.log("Run anyway");
+    }
+}
